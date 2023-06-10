2b15bd050d236fdf77161f4a879917af8358b963
Allan Oliveira <allan.oliveira@gpr.com.br>
2023-06-10 20:47:56 -0300

    Dev environment

---
+++ app/entrypoint-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
